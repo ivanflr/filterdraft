1f6a1bc45d7871d78a2ee55231cdcf42f11026e1
Adam Muro <bread@adams-air.home>
2018-05-01 18:47:11 -0400

    dolla

--- main.js
+++ main.js
@@ -3,26 +3,18 @@ $(document).ready(function(){
     var totalCost=0;
       $("input").each(function(){
         if($(this).prop('checked')){
-
           var thisCost = parseFloat($(this).val());
           totalCost=totalCost + thisCost;
         }
       });
       // var filter = $(this).val();
-
-
 //var totalCostMoneyFormat = format(totalCost,"$");
 //console.log(totalCostMoneyFormat);
-$(".TotalFines").find("span").text(totalCost);
-
-
+$(".TotalFines").find("span").text(format(totalCost, "$"));
     //  $(".item.active").removeClass("active");
-
       //$("." + filter).addClass("active");
-
-
   });
-  //function format(n, currency) {
-    //return currency + n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
-//}
+  function format(n, currency) {
+    return currency + n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+  }
 });
